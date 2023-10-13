180cde47a6d974e2d12ad1d96db3b2231921ec2b
Jean Pierre <hjepih@gmail.com>
2023-10-13 13:42:37 -0300

    fix: cors error

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const route = require("./route");
 const isAllowed = require("./middlewares/isAllowed");
 
 const app = express();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "999mb" }));
 app.use(isAllowed);
 app.use("/api/v1", route);
